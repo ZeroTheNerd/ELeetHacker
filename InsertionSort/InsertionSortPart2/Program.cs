ae87e029ca22a6d695a771eac87c957d0a348c7a
ZachMartim <zachmartim101@gmail.com>
2024-10-11 23:15:29 -0600

    InsertSort Part 2 completed

---
+++ InsertionSort/InsertionSortPart2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = 8;
+List<int> arr = [8, 7, 6, 5, 4, 3, 2, 1];
+//List<int> arr = [1, 4, 3, 5, 6, 2];
+
+static void insertionSort2(int n, List<int> arr)
+{
+    for(int i = 0; i < arr.Count - 1; i++)
+    {
+        if (arr[i+ 1] < arr[i])
+        {
+            for(int j = i + 1; j >= 1; j--) {
+                if (arr[j] < arr[j - 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+
+                //Console.WriteLine(string.Join(" ", arr));
+
+            }
+            //Console.WriteLine(string.Join(" ", arr));
+
+        }
+        Console.WriteLine(string.Join(" ", arr));
+    }
+}
+
+
+insertionSort2(n, arr);
