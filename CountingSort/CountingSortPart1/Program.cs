068bbf726935e8ec4e707a4e0f9e8076d2d75fbf
ZachMartim <zachmartim101@gmail.com>
2024-10-12 16:25:00 -0600

    CountingSort Part 1 - Completed

---
+++ CountingSort/CountingSortPart1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+static List<int> countingSort(List<int> arr)
+{
+    int min = arr.Min();
+    int max = arr.Max();
+    int range = (max - min) + 1;
+    List<int> numCount = new List<int>();
+    for (int i = 0; i < 100; i++)
+    {
+        numCount.Add(0);
+    }
+    for (int i = 0; i < arr.Count; i++)
+    {
+        numCount[arr[i]]++;
+    }
+    return numCount;
+
+}
