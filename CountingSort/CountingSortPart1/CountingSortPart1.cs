525060eca38f4c4f50cbb6df0f78733e3bdc3699
ZachMartim <zachmartim101@gmail.com>
2024-10-12 16:37:04 -0600

    Counting Sort Part 2 - Completed

--- CountingSort/CountingSortPart1/CountingSortPart1.cs
+++ CountingSort/CountingSortPart1/CountingSortPart1.cs
@@ -6,14 +6,26 @@ static List<int> countingSort(List<int> arr)
     int max = arr.Max();
     int range = (max - min) + 1;
     List<int> numCount = new List<int>();
+    List<int> orderedList = new List<int>();
+    //Initialize numCount List
     for (int i = 0; i < 100; i++)
     {
         numCount.Add(0);
     }
+    //Count the frequency of values
     for (int i = 0; i < arr.Count; i++)
     {
         numCount[arr[i]]++;
     }
-    return numCount;
-
+    //Create orderedList
+    for (int i = 0; i < numCount.Count; i++)
+    {
+        for (int j = 0; j < numCount[i]; j++)
+        {
+            orderedList.Add(i);
+        }
+    }
+    return orderedList;
 }
+
+
