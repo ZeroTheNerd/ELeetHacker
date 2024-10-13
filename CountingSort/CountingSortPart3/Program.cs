bcc8de15b08ec57648d45cfbc9f1d13d4b85096a
ZachMartim <zachmartim101@gmail.com>
2024-10-12 23:33:58 -0600

    Counting Sort Part 3 - Completed

--- CountingSort/CountingSortPart3/Program.cs
+++ CountingSort/CountingSortPart3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+    int n = int.Parse(Console.ReadLine());
+    List<int> numCount = new List<int>(new int[100]);
+    //input array
+    List<int> ar = new List<int>();
+
+    for (int i = 0; i < n; i++)
+    {
+        // Read the line
+        string input = Console.ReadLine();
+
+        // Split the input into the integer and string part
+        string[] parts = input.Split(' ');
+        int value = int.Parse(parts[0]); // The first part is the integer
+
+        // Increment the count for this value
+        numCount[value]++;
+    }
+    // for (int i = 0; i < 100; i++)
+    // {
+    //     numCount.Add(0);
+    // }
+    // for(int i = 0; i < ar.Count; i++) { 
+    //     numCount[ar[i]]++;
+
+    // }
+    int sum = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        sum += numCount[i];
+        Console.Write(sum + " ");
+    }
