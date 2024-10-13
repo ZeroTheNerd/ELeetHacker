fcac26d54d5873adf5a135a2381141c2ed198604
ZachMartim <zachmartim101@gmail.com>
2024-10-13 00:08:12 -0600

    Warmup - MinMax - Completed

---
+++ Warmup/MiniMaxSum/Program.cs
@@ -0,0 +1,13 @@
+﻿
+List<int> arr = [140638725, 436257910, 953274816, 734065819, 362748590];
+
+long max = arr.Max();
+long min = arr.Min();
+long sum = 0;
+for (int i = 0; i < arr.Count; i++)
+{
+    sum += arr[i];
+}
+long minSum = sum - max;
+long maxSum = sum - min;
+Console.WriteLine(minSum + " " + maxSum);
