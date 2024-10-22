5243b48527d00fde932ecb591c1877a1c4a6063e
ZachMartim <zachmartim101@gmail.com>
2024-10-21 21:36:42 -0600

    Formatting

--- Java/src/JavaLoopsII.java
+++ Java/src/JavaLoopsII.java
@@ -7,16 +7,16 @@ public class JavaLoopsII {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         for (int i = 0; i < n; i++) {
-        int start = scanner.nextInt();
-        int mult = scanner.nextInt();
-        int n2 = scanner.nextInt();
-        int sum = start;
-        ;
-        for (int j = 0; j <= n2 - 1; j++) {
-            sum += Math.pow(2, j) * mult;
-            System.out.print(sum + " ");
-        }
+            int start = scanner.nextInt();
+            int mult = scanner.nextInt();
+            int n2 = scanner.nextInt();
+            int sum = start;
+
+            for (int j = 0; j <= n2 - 1; j++) {
+                sum += Math.pow(2, j) * mult;
+                System.out.print(sum + " ");
+            }
         System.out.println();
-    }
+        }
     }
 }
